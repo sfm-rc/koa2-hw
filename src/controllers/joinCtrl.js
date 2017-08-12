3239ce3df68a9d04a88b9197215d984f0014a956
suyuan <suyuan1573@gmail.com>
2017-08-12 17:20:01 +0800

    join interface

--- src/controllers/joinCtrl.js
+++ src/controllers/joinCtrl.js
@@ -15,31 +15,38 @@ const join = async(ctx, next) => {
   await query('set autocommit=0;');
   await query('begin;');
   try{
-      console.info('lock for activity_id', activity_id);
-    const activities = await query('select * from hw_activity where id=? for update;', [activity_id]);// activity_id这行加了行锁
-    const activity = activities[0];
-    if(activity.cur_num<activity.limit_num){
-        // 可以报名
-        const sql = `INSERT into hw_join VALUES(
-            NULL,'${user_name}', '${user_name_alias}', '${sex}', '${mobile}', 
-            '${down_payment}', '${extra}', '${activity_id}', '${getLinuxTimeStamp()}'
-        )`;
-        var data = await query(sql);
-        console.log('insert hw_join for join', data);
-        if(data.insertId>0){
-            const update_res = await query('UPDATE hw_activity set cur_num=cur_num+1 WHERE id=?', [activity_id]);
-            console.log('update set ++1', update_res);
-            ctx.body = {code: 0, message:'报名成功', data}
-        }
+    const joinUser = await query('select count(*) as count from hw_join where activity_id=? and mobile=?', [activity_id, mobile]);
+    console.log('joinUser---', joinUser);
+    if(joinUser[0].count>0){
+        ctx.body = {code: 110, message:'您已经报过名', data:{}}
     }
     else{
-        ctx.body = {code: 110, message:'报名失败', data:'人数超过显示'}
-        
+        console.info('lock for activity_id', activity_id);
+        const activities = await query('select * from hw_activity where id=? for update;', [activity_id]);// activity_id这行加了行锁
+        const activity = activities[0];
+        if(activity.cur_num<activity.limit_num){
+            // 可以报名
+            const sql = `INSERT into hw_join VALUES(
+                NULL,'${user_name}', '${user_name_alias}', '${sex}', '${mobile}', 
+                '${down_payment}', '${extra}', '${activity_id}', '${getLinuxTimeStamp()}'
+            )`;
+            var data = await query(sql);
+            console.log('insert hw_join for join', data);
+            if(data.insertId>0){
+                const update_res = await query('UPDATE hw_activity set cur_num=cur_num+1 WHERE id=?', [activity_id]);
+                console.log('update set ++1', update_res);
+                ctx.body = {code: 0, message:'报名成功', data}
+            }
+        }
+        else{
+            ctx.body = {code: 110, message:'报名失败,活动人数受限制', data:{}}
+        }
     }
   }
   catch(error){
     console.error('join fail', error);
     await query('ROLLBACK');
+    ctx.body = {code: 110, message:'报名失败,未知异常', data:{}}
   }
   await query('commit;');
 
